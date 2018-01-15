5fa25e11876055d4f69028454aeb535be144add5
Mohammed Shurrab <devmsh87@gmail.com>
2018-01-16 01:25:38 +0200

    use SetUp and Helper function to make the code more clear

--- tests/Unit/FailedCourses.php
+++ tests/Unit/FailedCourses.php
@@ -15,51 +15,56 @@ class FailedCourses extends TestCase
 {
     use DatabaseMigrations;
 
+    protected function setUp()
+    {
+        parent::setUp();
+
+        Notification::fake();
+    }
+
+    private function generateInterests($min_candidates, $start_date, $interests_count)
+    {
+        $this->course = factory(Course::class)->create([
+            'min_candidates' => $min_candidates
+        ]);
+
+        $this->timeslot = factory(Timeslot::class)->create([
+            'start_date' => $start_date
+        ]);
+
+        $this->interests = factory(Interest::class, $interests_count)->create([
+            'course_id' => $this->course->id,
+            'timeslot_id' => $this->timeslot->id
+        ]);
+    }
+
     /**
      * Based on https://github.com/devmsh/tdl/issues/2
      * Scenario 1
      */
     public function test_system_send_notification_when_exceed_deadline()
     {
-        $course = factory(Course::class)->create([
-            'min_candidates' => 10
-        ]);
-
-        $timeslot = factory(Timeslot::class)->create([
-            'start_date' => Carbon::now()->addDays(Timeslot::VALIDATE_BEFORE)
-        ]);
-
-        factory(Interest::class, 9)->create([
-            'course_id' => $course->id,
-            'timeslot_id' => $timeslot->id
-        ]);
-
-        Notification::fake();
+        $this->generateInterests(
+            10,
+            Carbon::now()->addDays(Timeslot::VALIDATE_BEFORE),
+            9
+        );
 
         Timeslot::first()->validate();
 
-        Notification::assertSentTo($course, DeadlineExceededNotification::class);
+        Notification::assertSentTo($this->course, DeadlineExceededNotification::class);
     }
 
     public function test_system_not_send_notification_when_not_exceed_deadline()
     {
-        $course = factory(Course::class)->create([
-            'min_candidates' => 10
-        ]);
-
-        $timeslot = factory(Timeslot::class)->create([
-            'start_date' => Carbon::yesterday()
-        ]);
-
-        factory(Interest::class, 9)->create([
-            'course_id' => $course->id,
-            'timeslot_id' => $timeslot->id
-        ]);
-
-        Notification::fake();
+        $this->generateInterests(
+            10,
+            Carbon::yesterday(),
+            9
+        );
 
         Timeslot::first()->validate();
 
-        Notification::assertNotSentTo($course, DeadlineExceededNotification::class);
+        Notification::assertNotSentTo($this->course, DeadlineExceededNotification::class);
     }
 }
