df81ba857048d2ccaac332d53bb43f001b5b136f
Mohammed Shurrab <devmsh87@gmail.com>
2018-01-30 21:37:49 +0200

    assertCommandScheduled support

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,24 @@
 
 namespace Tests;
 
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function assertCommandScheduled($command, $every = null)
+    {
+        /** @var Schedule $scheduler */
+        $scheduler = app()->make(Schedule::class);
+
+        $events = collect($scheduler->events());
+        $filteredEvents = $events->filter(function($event) use($command){
+            return strstr($event->command,$command);
+        });
+
+        $this->assertGreaterThan(0,$filteredEvents->count(),
+            "Command $command not scheduled");
+    }
 }
