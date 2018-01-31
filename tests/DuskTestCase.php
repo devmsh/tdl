5ee0d7ce0e9fdb601316ab77ffd36a4bdc43858a
Mohammed Shurrab <devmsh87@gmail.com>
2018-01-31 14:30:48 +0200

    TBD

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -2,14 +2,69 @@
 
 namespace Tests;
 
+use Illuminate\Support\Facades\Artisan;
 use Laravel\Dusk\TestCase as BaseTestCase;
 use Facebook\WebDriver\Chrome\ChromeOptions;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\ProcessBuilder;
+
+trait RunsWebServer
+{
+    /**
+     * @var Process
+     */
+    protected static $webServerProcess;
+
+    /**
+     * Start the web server process
+     */
+    public static function startWebServer()
+    {
+        static::$webServerProcess = static::buildServerProcess();
+        static::$webServerProcess->start();
+        static::afterClass(function() {
+            static::stopWebServer();
+        });
+    }
+
+    /**
+     * Stop the web server process
+     */
+    public static function stopWebServer()
+    {
+        if (static::$webServerProcess) {
+            static::$webServerProcess->stop();
+        }
+    }
+
+    /**
+     * Build the process to run the web server
+     *
+     * @return \Symfony\Component\Process\Process
+     * @throws \Symfony\Component\Process\Exception\InvalidArgumentException
+     * @throws \Symfony\Component\Process\Exception\LogicException
+     */
+    protected static function buildServerProcess()
+    {
+        return (new ProcessBuilder())
+            ->setTimeout(null)
+            ->setWorkingDirectory(realpath(__DIR__.'/../'))
+            ->add(PHP_BINARY)
+            ->add('artisan')
+            ->add('serve')
+            ->add('--env=dusk')
+            ->add('--host=127.0.0.1')
+            ->add('--port=8000')
+            ->getProcess();
+    }
+}
+
 
 abstract class DuskTestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication,RunsWebServer;
 
     /**
      * Prepare for Dusk test execution.
@@ -19,6 +74,7 @@ abstract class DuskTestCase extends BaseTestCase
      */
     public static function prepare()
     {
+        static::startWebServer();
         static::startChromeDriver();
     }
 
