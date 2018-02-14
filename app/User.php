12d9380937266293414bd622bec5a1fb1557a782
Mohammed Shurrab <devmsh87@gmail.com>
2018-02-14 17:48:14 +0200

    fix the div function

--- app/User.php
+++ app/User.php
@@ -8,7 +8,7 @@ use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens,Notifiable;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
